0dbbf7d8527aed1f25dbd555c4982ecaec9f5fbe
Teemu Laine <teemu.laine@etteplan.com>
2021-06-18 08:25:11 +0300

    Santa challenge file reading

--- santachallenge/Program.cs
+++ santachallenge/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 namespace santachallenge
 {
@@ -6,7 +10,29 @@ namespace santachallenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Reading nicelist...");
+            var input = utils.FileSystem.GetInput("nicelist", "santachallenge");
+            var culture = CultureInfo.CurrentCulture;
+            
+            // (index,latitude,longitude,weight)
+            // longitude - itä-länsi
+            var nicelist = new List<(string, double, double, int)>();
+
+            foreach (var row in input)
+            {
+                var content = row.Split(";");
+                nicelist.Add((
+                    content[0],
+                    double.Parse(content[1].Replace('.', ',')),
+                    double.Parse(content[2].Replace('.', ',')),
+                    int.Parse(content[3])
+                    ));
+            }
+
+            var minLatitude = nicelist.Min(item => item.Item2);
+            var maxLatitude = nicelist.Max(item => item.Item2);
+            var minLongitude = nicelist.Min(item => item.Item3);
+            var maxLongitude = nicelist.Max(item => item.Item3);
         }
     }
 }
